91b53937d4a53718df1ae4d82f050c3a11fb2661
John Villalino <johndominicvillalino@outlook.com>
2021-09-21 20:02:35 +0800

    add cli args with handlers and console the retured jokes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 // jokes data
 const jokes = [{
     message: 'Why was the math teacher late to work? She took the rhombus.'
@@ -11,37 +10,52 @@ const jokes = [{
 
     message: `What did Yoda say when he saw himself in 4K? "HDMI.`
 }, {
-    message: `I'd lzxczxcen shoe?`
+    message: `Why did the kid throw a stick of butter out the window? A: To see butter-fly`
 }
-, {
-    message: `I'd likeasdasday. Wooden shoe?`
+    , {
+    message: `Why didn’t the teddy bear eat dessert? He was stuffed`
 }
-, {
-    message: `I'd like sddzxcay. Wooden shoe?`
+    , {
+    message: `What do you give a sick lemon? A Lemon-aid`
 }
-, {
-    message: `I'd like aasdzxcWooden shoe?`
+    , {
+    message: `Why do you smear peanut butter in the road? To go with the traffic jam`
 }
-, {
-    message: `I'd asdasdzxczxchoe?`
+    , {
+    message: `What do you call an attractive fruit? A fine-apple`
 }
 ]
 
-// random picker
+// @param numberOfJokes is a number of jokes needed by the user
+// @@return number of jokes needed randomly picked from the jokes data
 
-//logs the randomly picked jokes
+const aRandomJoke = (numberOfJokes) => {
 
-const aRandomJoke = () => {
+    //@checker
+    if (numberOfJokes > jokes.length) {
+        return `We have only ${jokes.length} jokes here, sorry.`
+    }
+    if (numberOfJokes < 1) {
+        return "Okay, I'm done! Google it yourself!"
+    }
+    if (typeof numberOfJokes !== "number" ||  isNaN(numberOfJokes)) {
+        return "Give me a number!!"
+    }
+    //@@ end of Checker @@
 
+
+    //@ store return array of jokes
+    const jokesArr = [];
     const numOfJokes = jokes.length;
 
+    //@ random number function
     const randomNumGenerator = () => {
         return Math.floor(Math.random() * numOfJokes);
     }
 
     const randomArr = []
 
-    //push random namber within the range of data length 
+    //push random number within the range of data length 
     while (randomArr.length !== jokes.length) {
         let picker = randomNumGenerator();
         if (!randomArr.includes(picker)) {
@@ -49,9 +63,8 @@ const aRandomJoke = () => {
         }
     }
 
-    //console.log(randomArr)
-
-    const logFunction = jokes.map(data => {
+    //@ map the jokes to add each a getter
+    const jokesWithFunction = jokes.map(data => {
 
         return {
             _message: data.message,
@@ -61,12 +74,30 @@ const aRandomJoke = () => {
         }
     })
 
-    //console log all jokes
-    for (let i = 0; i < randomArr.length; i++) {
-        console.log(logFunction[randomArr[i]].joke)
+    for(let i = 0; i < numberOfJokes ; i++){
+        jokesArr.push(jokesWithFunction[randomArr[i]].joke)
     }
+
+    //logs in console invoking.
+
+    console.log(`\nHope you enjoy these jokes. If not, you need s*x! `);
+
+    for(let i = 0; i < numberOfJokes ; i++){
+        console.log(`\n${i+1}. ${jokesWithFunction[randomArr[i]].joke} `)
+    }
+    console.log(`\n`);
+
+    //returns an array of jokes
+    return jokesArr;
 
 }
 
-aRandomJoke();
+// @desc capture cmd args 
+let args = process.argv[2]
+//convert to int
+args = +args
+
+aRandomJoke(args)
+
+
 
